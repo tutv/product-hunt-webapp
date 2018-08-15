4262237f59d5c9a1f00421ba9a92cb70b9664804
Max <tutv95@gmail.com>
2018-08-15 23:05:33 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1 style={{textAlign: 'center'}}>Product Hunt</h1>
+            </div>
+        )
+    }
+}
+
+export default App

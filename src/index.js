5ce321716377a95191e7fc22b5c4b29e435db2be
Max <tutv95@gmail.com>
2018-08-17 23:43:30 +0700

    Config sacss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './app/App'
+import './scss/app.scss'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
